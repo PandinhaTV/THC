0810e9db23aa587a4dd7ce939a17b5f3c53817c5
PandinhaTV <57328243+PandinhaTV@users.noreply.github.com>
2025-05-30 04:25:42 +0100

    Added Enemy And add added finger gun

--- Assets/Team/Diogo/FireGun.cs
+++ Assets/Team/Diogo/FireGun.cs
@@ -3,18 +3,30 @@ using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
-
-[RequireComponent(typeof(XRGrabInteractable))]
-public class FireGun : MonoBehaviour 
-{
-    [Header("VR Settings")] public XRNode controllerNode;
-    public Transform muzzle;
+using UnityEngine.InputSystem;
+public class FireGun : MonoBehaviour
+    {
+        
     public GameObject bulletPrefab;
     public float bulletSpeed = 20f;
     public AudioSource gunShotSound;
+    public float fireRate = 0.2f;
+    public InputActionProperty triggerAction;
+    public Transform muzzle;
+    
+    
+    private float nextFireTime = 0f;
 
-    private UnityEngine.XR.Interaction.Toolkit.Interactors.XRBaseInputInteractor interactor;
+    
 
+    public void Update()
+    {
+        if (triggerAction.action.ReadValue<float>() > 0.1f && Time.time >= nextFireTime)
+        {
+            Fire();
+            nextFireTime = Time.time + fireRate;
+        }
+    }
     public void Fire()
     {
         if (bulletPrefab != null && muzzle != null)
@@ -22,27 +34,12 @@ public class FireGun : MonoBehaviour
             GameObject bullet = Instantiate(bulletPrefab, muzzle.position, muzzle.rotation);
             Rigidbody rb = bullet.GetComponent<Rigidbody>();
             rb.linearVelocity = muzzle.forward * bulletSpeed;
-
             gunShotSound?.Play();
         }
-    }
 
-    private void OnEnable()
-    {
-        var interactable = GetComponent<XRGrabInteractable>();
-        interactable.activated.AddListener(OnActivate);
-    }
-
-    private void OnDisable()
-    {
-        var interactable = GetComponent<XRGrabInteractable>();
-        interactable.activated.RemoveListener(OnActivate);
-    }
-
-    private void OnActivate(ActivateEventArgs args)
-    {
-        Fire();
+            
     }
 }
+
 
 
